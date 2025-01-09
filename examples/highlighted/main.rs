12c3a752b195e254e2602e64a5e535cbfd15cd90
altacountbabi <altacountbabi@users.noreply.github.com>
2025-01-09 15:15:21 +0200

    Fix `highlighted` example.

--- examples/highlighted/main.rs
+++ examples/highlighted/main.rs
@@ -4,11 +4,11 @@ fn main() {
     logger::panic::set_panic_hook();
     let location = Location::from_path("examples/highlighted/bad_code.rs", 1..=1)
         .expect("Failed to read file")
-        .section(21..=22);
+        .section(20..=22);
 
     warn!(
         "Using new line in println! call",
         location: location,
-        hint: format!("Remove {} in {} call", "\\n".bold(), "println!".bold())
+        hint: format!("Remove {} in {} call", "'\\n'".bold(), "'println!'".bold())
     );
 }
